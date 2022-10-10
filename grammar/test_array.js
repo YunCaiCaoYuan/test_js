b40eacfb1c66f5b621a05772699d47e8ba623dc4
sunbin <yourName@gmail.com>
2022-10-10 17:10:57 +0800

    数组语法

---
+++ grammar/test_array.js
@@ -0,0 +1,26 @@
+// var str="Welcome to here ！";
+// var n=Array.from(str);
+// console.log(n);
+
+// var str="1,8";
+// var n=Array.from(str);
+// console.log(n);
+
+// var str="Welcome to here ！";
+// var n=str.split("");
+// console.log(n);
+
+// var str="1,8";
+// var n=str.split(",");
+// console.log(n);
+
+//  JS 数组中元素转换类型
+// let arr = [1, 2, 3] ;
+// arr = arr.map(String);
+// console.log(arr) // [ '1', '2', '3']
+
+let arr = [ '1', '2', '3'];
+arr = arr.map(Number);
+console.log(arr) // [1, 2, 3]
+
+
